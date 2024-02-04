124f956b26b463d721679d62af3e85e2a62124b3
MoralesTomas <saquic.ttomas@gmail.com>
2024-02-04 00:11:42 -0600

    [add] se le dio estilo a la parte de actualizar/editar producto

--- frontend/src/CRUD/Producto/ActualizarProducto.jsx
+++ frontend/src/CRUD/Producto/ActualizarProducto.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import axios from 'axios';
 import serverUrl from '../../ServerConfig';
 
 const ActualizarProducto = () => {
-
     const { idProducto } = useParams();
     const [producto, setProducto] = useState(null);
     const [marcas, setMarcas] = useState([]);
@@ -77,161 +76,196 @@ const ActualizarProducto = () => {
             <h1 style={{ marginLeft: '1rem' }}>Editar Producto</h1>
             <div className="container mt-4">
                 <form>
-                    <div className="form-group">
-                        <label htmlFor="codigo">Código:</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="codigo"
-                            name="codigo"
-                            value={producto.codigo}
-                            onChange={handleInputChange}
-                        />
+                    <div className="row">
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <label htmlFor="codigo">Código:</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="codigo"
+                                    name="codigo"
+                                    value={producto.codigo}
+                                    onChange={handleInputChange}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <label htmlFor="descripcionProducto">Descripción:</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="descripcionProducto"
+                                    name="descripcionProducto"
+                                    value={producto.descripcionProducto}
+                                    onChange={handleInputChange}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <label htmlFor="precio">Precio:</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="precio"
+                                    name="precio"
+                                    value={producto.precio}
+                                    onChange={handleInputChange}
+                                />
+                            </div>
+                        </div>
                     </div>
 
-                    <div className="form-group">
-                        <label htmlFor="descripcionProducto">Descripción:</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="descripcionProducto"
-                            name="descripcionProducto"
-                            value={producto.descripcionProducto}
-                            onChange={handleInputChange}
-                        />
+                    <div className="row">
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <label htmlFor="stock">Stock:</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="stock"
+                                    name="stock"
+                                    value={producto.stock}
+                                    onChange={handleInputChange}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <label htmlFor="iva">IVA:</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="iva"
+                                    name="iva"
+                                    value={producto.iva}
+                                    onChange={handleInputChange}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <label htmlFor="peso">Peso:</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="peso"
+                                    name="peso"
+                                    value={producto.peso}
+                                    onChange={handleInputChange}
+                                />
+                            </div>
+                        </div>
                     </div>
 
-                    <div className="form-group">
-                        <label htmlFor="precio">Precio:</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="precio"
-                            name="precio"
-                            value={producto.precio}
-                            onChange={handleInputChange}
-                        />
+                    <div className="row">
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <label htmlFor="activo">Activo:</label>
+                                <select
+                                    className="form-control"
+                                    id="activo"
+                                    name="activo"
+                                    value={producto.activo ? 'true' : 'false'}
+                                    onChange={handleInputChange}
+                                >
+                                    <option value="true">Sí</option>
+                                    <option value="false">No</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <label htmlFor="marca">Marca:</label>
+                                <select
+                                    className="form-control"
+                                    id="marca"
+                                    name="idMarca"
+                                    value={producto.idMarca}
+                                    onChange={handleInputChange}
+                                >
+                                    {marcas.map(marca => (
+                                        <option key={marca.idMarca} value={marca.idMarca}>
+                                            {marca.descripcion}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <label htmlFor="proveedor">Proveedor:</label>
+                                <select
+                                    className="form-control"
+                                    id="proveedor"
+                                    name="idProveedor"
+                                    value={producto.idProveedor}
+                                    onChange={handleInputChange}
+                                >
+                                    {proveedores.map(proveedor => (
+                                        <option key={proveedor.idProveedor} value={proveedor.idProveedor}>
+                                            {proveedor.descripcion}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                        </div>
                     </div>
 
-                    <div className="form-group">
-                        <label htmlFor="stock">Stock:</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="stock"
-                            name="stock"
-                            value={producto.stock}
-                            onChange={handleInputChange}
-                        />
-                    </div>
+                    <div className="row">
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <label htmlFor="presentacion">Presentación:</label>
+                                <select
+                                    className="form-control"
+                                    id="presentacion"
+                                    name="idPresentacion"
+                                    value={producto.idPresentacion}
+                                    onChange={handleInputChange}
+                                >
+                                    {presentaciones.map(presentacion => (
+                                        <option key={presentacion.idPresentacion} value={presentacion.idPresentacion}>
+                                            {presentacion.descripcion}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                        </div>
 
-                    <div className="form-group">
-                        <label htmlFor="iva">IVA:</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="iva"
-                            name="iva"
-                            value={producto.iva}
-                            onChange={handleInputChange}
-                        />
+                        <div className="col-md-4">
+                            <div className="form-group">
+                                <label htmlFor="zona">Zona:</label>
+                                <select
+                                    className="form-control"
+                                    id="zona"
+                                    name="idZona"
+                                    value={producto.idZona}
+                                    onChange={handleInputChange}
+                                >
+                                    {zonas.map(zona => (
+                                        <option key={zona.idZona} value={zona.idZona}>
+                                            {zona.descripcion}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                        </div>
                     </div>
+                    <br />
 
-                    <div className="form-group">
-                        <label htmlFor="peso">Peso:</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="peso"
-                            name="peso"
-                            value={producto.peso}
-                            onChange={handleInputChange}
-                        />
-                    </div>
-
-                    <div className="form-group">
-                        <label htmlFor="activo">Activo:</label>
-                        <select
-                            className="form-control"
-                            id="activo"
-                            name="activo"
-                            value={producto.activo ? 'true' : 'false'}
-                            onChange={handleInputChange}
-                        >
-                            <option value="true">Sí</option>
-                            <option value="false">No</option>
-                        </select>
-                    </div>
-
-                    <div className="form-group">
-                        <label htmlFor="marca">Marca:</label>
-                        <select
-                            className="form-control"
-                            id="marca"
-                            name="idMarca"
-                            value={producto.idMarca}
-                            onChange={handleInputChange}
-                        >
-                            {marcas.map(marca => (
-                                <option key={marca.idMarca} value={marca.idMarca}>
-                                    {marca.descripcion}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-
-                    <div className="form-group">
-                        <label htmlFor="proveedor">Proveedor:</label>
-                        <select
-                            className="form-control"
-                            id="proveedor"
-                            name="idProveedor"
-                            value={producto.idProveedor}
-                            onChange={handleInputChange}
-                        >
-                            {proveedores.map(proveedor => (
-                                <option key={proveedor.idProveedor} value={proveedor.idProveedor}>
-                                    {proveedor.descripcion}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-
-                    <div className="form-group">
-                        <label htmlFor="presentacion">Presentación:</label>
-                        <select
-                            className="form-control"
-                            id="presentacion"
-                            name="idPresentacion"
-                            value={producto.idPresentacion}
-                            onChange={handleInputChange}
-                        >
-                            {presentaciones.map(presentacion => (
-                                <option key={presentacion.idPresentacion} value={presentacion.idPresentacion}>
-                                    {presentacion.descripcion}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-
-                    <div className="form-group">
-                        <label htmlFor="zona">Zona:</label>
-                        <select
-                            className="form-control"
-                            id="zona"
-                            name="idZona"
-                            value={producto.idZona}
-                            onChange={handleInputChange}
-                        >
-                            {zonas.map(zona => (
-                                <option key={zona.idZona} value={zona.idZona}>
-                                    {zona.descripcion}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-
-                    <button type="button" className="btn btn-primary" onClick={handleUpdateProducto}>
+                    <a href="/administrar-productos" className="btn btn-primary" >
+                        Regresar
+                    </a>
+                    
+                    <button type="button" className="btn btn-success" onClick={handleUpdateProducto}>
                         Actualizar Producto
                     </button>
                 </form>
